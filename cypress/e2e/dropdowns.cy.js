3fa81ff92a2c729852f34a01027b68caea911f8c
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2023-03-29 22:39:54 +0200

    Dropdowns update

--- cypress/e2e/dropdowns.cy.js
+++ cypress/e2e/dropdowns.cy.js
@@ -2,5 +2,23 @@ describe("Action to select option from the dropdown list", () => {
   it("Select option from dropdown list", () => {
     cy.visit("/");
     cy.contains(".sf-with-ul", "Women").click();
+
+    //Select with a name
+    cy.get("#selectProductSort").select("In stock");
+    //Select with value
+    cy.get("#selectProductSort").select("price:desc");
+    //Select with index
+    cy.get("#selectProductSort").select(4);
+  });
+
+  //Select one by one
+  it.only("Selection one by one", () => {
+    cy.visit("/");
+    cy.contains(".sf-with-ul", "Women").click();
+    cy.get("#selectProductSort").then((select) => {
+      cy.wrap(select)
+        .find("option")
+        .each((opcja) => cy.wrap(select).select(opcja.text));
+    });
   });
 });
