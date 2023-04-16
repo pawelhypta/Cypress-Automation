1acc7915b342d3e0239e5a69b9ff0d39f6f7d371
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2023-04-16 22:21:22 +0200

    Triggers final

--- cypress/e2e/trigger.cy.js
+++ cypress/e2e/trigger.cy.js
@@ -1,7 +1,8 @@
 describe("E2E - scroll/hover over", () => {
-  it("Hover over element", () => {
+  it("Hover over and scroll to the element", () => {
     cy.visit("/");
     cy.get(`a[title="Dresses"]`).eq(1).trigger("focus");
-    cy.get(`li.sfHover a[title="Summer Dresses"]`);
+    cy.get(`li.sfHover a[title="Summer Dresses"]`).click();
+    cy.get(`a[title="Specials"]`).eq(0).scrollIntoView();
   });
 });
