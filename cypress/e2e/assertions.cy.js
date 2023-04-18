187441f0c707948f69f21e8f54848ca6946c6a35
Paweł Hypta <69308367+pawelhypta@users.noreply.github.com>
2023-04-18 23:41:04 +0200

    Assertions by class and if visible

--- cypress/e2e/assertions.cy.js
+++ cypress/e2e/assertions.cy.js
@@ -10,5 +10,22 @@ describe("E2E - assertions", () => {
 
     //Text verification not contain
     cy.get(`a[title="Contact us"]`).should("not.contain", "Sign in");
+    cy.get(`a[title="Contact us"]`).then((b) => {
+      expect(b).not.to.contain("Sign in");
+    });
+
+    //Class cerification
+    cy.get("#search_query_top").should(
+      "have.class",
+      "search_query form-control ac_input"
+    );
+    cy.get("#search_query_top").then((searchBar) => {
+      expect(searchBar).to.have.class("search_query form-control ac_input");
+    });
+    //If the element is visible
+    cy.get("#search_query_top").should("be.visible");
+    cy.get("#search_query_top").then((searchBar) => {
+      expect(searchBar).to.be.visible;
+    });
   });
 });
